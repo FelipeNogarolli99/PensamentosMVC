f644a8cdd6311701ad78aa3e4b9f3d31827314de
Felipe Nogarolli Macedo <felipenogarolli@hotmail.com>
2024-10-07 16:41:33 -0300

    teste

--- controllers/PensamentosController.js
+++ controllers/PensamentosController.js
@@ -4,5 +4,6 @@ const User = require('../models/User')
 module.exports = class PensamentosController{
     static async showPensamentos(req, res){
         res.render('pensamentos/home')
+        
     }
 }
